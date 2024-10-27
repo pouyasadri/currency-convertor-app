16de1d835a3a56db03c51d039d241bf3995a7966
pouyasadri <pouyapaghandeh78@gmail.com>
2024-10-27 10:52:04 +0100

    Initial Commit

---
+++ CurrencyConvertorApp/CurrencyConvertorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConvertorAppApp.swift
+//  CurrencyConvertorApp
+//
+//  Created by Pouya Sadri on 27/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConvertorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
